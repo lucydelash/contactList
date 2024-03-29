41a28d504cc2485ed1d06352823f04763f13fe5d
lucydelash <lucyrudd614@gmail.com>
2024-03-29 15:28:40 -0400

    pushing 2nd tab

--- contact-list/src/App.jsx
+++ contact-list/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import ContactList from './components/ContactList'; // Importing the ContactList component
 
 function App() {
-  const [] = useState(0)
+  const [] = useState(0);
 
   return (
     <>
+      <ContactList /> {/* Using the ContactList component within the React fragments */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
