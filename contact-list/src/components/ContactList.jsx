13871d71d6d6267ac1435c75b93b0d244c402c84
lucydelash <lucyrudd614@gmail.com>
2024-03-29 15:40:07 -0400

    pushing 3rd tab

--- contact-list/src/components/ContactList.jsx
+++ contact-list/src/components/ContactList.jsx
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ContactRow from './ContactRow';
 
-const dummyContacts = [
-  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-];
-
 export default function ContactList() {
-  const [contacts, setContacts] = useState(dummyContacts);
+  const [contacts, setContacts] = useState([]);
 
-  console.log("Contacts: ", contacts);
+  useEffect(() => {
+    async function fetchContacts() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const result = await response.json();
+        setContacts(result);
+        console.log("Contacts from API: ", result);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchContacts();
+  }, []);
+
+  console.log("Contacts state variable: ", contacts);
 
   return (
     <table>
